f3aa1be56b2443acd1c205cedd6910822d7aa29e
Joshua Lee <joshualee@iconfigure.org>
2016-11-16 23:06:34 -1000

    answered all the way down to append new nodes

--- linkedList.js
+++ linkedList.js
@@ -5,4 +5,65 @@
  */
 function linkedListGenerator(){
 
+  var head = null;
+
+  function getHead(){
+    return head;
+
+  }
+
+  function getTail () {
+    var currentNode = head;
+
+    while (currentNode !== null) {
+
+      if(currentNode.next !== null){
+        currentNode = currentNode.next;
+
+      }else{
+
+        return currentNode;
+
+      }
+    }
+    return currentNode;
+
+  }
+
+  function add (addValue) {
+    var myTail = getTail();
+    var newNode = {
+      value: addValue,
+      next: null
+    }
+
+    return newNode;
+
+  }
+
+  function remove() {
+
+  }
+
+  function get () {
+
+  }
+
+  function insert () {
+
+  }
+
+  return {
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    remove: remove,
+    get: get,
+    insert: insert
+  };
+
 }
+
+
+let ll = linkedListGenerator();
+console.log(ll);
